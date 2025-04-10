b36f843e06b283594c0fd7ad13ac770219bf678a
Yael <yael.castillo.dev@gmail.com>
2025-04-10 11:27:34 -0600

    Estructura base

---
+++ src/main/java/company/proyectoaerolineas/Proyectoaerolineas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package company.proyectoaerolineas;
+
+/**
+ *
+ * @author yael
+ */
+public class Proyectoaerolineas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
